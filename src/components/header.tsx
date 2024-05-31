25b1e11dfd04407ef4546f9a7fe8eb978c122b59
githiomi <danielgithiomi@gmail.com>
2024-05-31 20:38:35 +0400

    feat: added and designed header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,36 +1,32 @@
-import { Avatar, Box, Flex, Spacer, Text } from "@chakra-ui/react";
-import { useState } from "react"
+import { Avatar, Flex, Spacer, Text } from "@chakra-ui/react";
 import { FaSun } from 'react-icons/fa'
 
 const HeaderComponent = () => {
 
-   const [authState, setAuthState] = useState(false);
    const profile_picture = "/profile_picture.png"
    const username = "Daniel Githiomi"
 
    return (
 
-      <Flex className="w-full px-8 flex justify-between">
+      <Flex className="w-full flex justify-between mb-8">
 
-         <h1 className="text-2xl uppercase tracking-wide" onClick={() => setAuthState((current) => !current)}>Blog Post Manager</h1>
+         <h1 className="text-3xl uppercase tracking-wide font-semibold">Blog Post Manager</h1>
 
          <Spacer />
 
-         <div className="flex items-center">
+         <Flex alignItems='center'>
 
-            <FaSun className="text-blue-300 text-2xl hover:text-blue-400 cursor-pointer duration-200 ease-in-out hover:ease-in" />
+            <FaSun className="text-blue-300 text-3xl hover:text-blue-400 cursor-pointer duration-200 ease-in-out hover:ease-in mx-10" />
 
-            {authState &&
-               <Box alignContent="center">
-                  <Avatar name={username} src={profile_picture} />
+            <Flex alignItems='center' gap='1rem'>
+               <Avatar name={username} src={profile_picture} />
 
-                  <Text>Daniel Githiomi</Text>
-               </Box>
-            }
+               <Text className="uppercase font-semibold" >{username}</Text>
+            </Flex>
 
-         </div>
+         </Flex>
 
-      </Flex>
+      </Flex >
    )
 }
 
